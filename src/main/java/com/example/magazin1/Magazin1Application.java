0691226a6527c57e41168c39da79b961cc9fd25e
Kirill <121629113+kirKirKir22@users.noreply.github.com>
2024-01-24 16:19:48 +0300

    init

---
+++ src/main/java/com/example/magazin1/Magazin1Application.java
@@ -0,0 +1,13 @@
+package com.example.magazin1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Magazin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Magazin1Application.class, args);
+	}
+
+}
